ba32d4afa9aea97321ce1488fee6944848b191c4
Lokecrafter <66024236+Lokecrafter@users.noreply.github.com>
2025-12-26 05:09:19 +0100

    Working

--- src/lidar.cpp
+++ src/lidar.cpp
@@ -5,6 +5,7 @@
 # include "freertos/semphr.h"
 # include "driver/gpio.h"
 # include "driver/i2c_master.h"
+# include <string>
 
 
 # define LIDAR_INTERRUPT_PIN GPIO_NUM_13
@@ -20,22 +21,22 @@ void write_to_lidar_register(uint8_t reg, uint8_t value){
     uint8_t data[2] = {reg, value};
     i2c_master_transmit(lidar_dev, data, 2, TIMEOUT_MS);
 }
-void read_from_lidar_register(uint8_t reg, uint8_t* buffer, size_t buffer_length){
-    i2c_master_transmit_receive(lidar_dev, &reg, 1, buffer, buffer_length, TIMEOUT_MS);
+uint8_t read_from_lidar_register(uint8_t reg){
+    uint8_t buffer = 0;
+    i2c_master_transmit_receive(lidar_dev, &reg, 1, &buffer, 1, TIMEOUT_MS);
+    return buffer;
 }
 void start_distance_measurement(bool bias_correction){
     # define ACQ_COMMAND_REG 0x00
     if(bias_correction)     write_to_lidar_register(ACQ_COMMAND_REG, 0x04); // with receiver bias correction
     else                    write_to_lidar_register(ACQ_COMMAND_REG, 0x03); // without
 }
-uint16_t read_distance_measurement(){
-    // Read distance
-    # define HIGH_BYTE_REG 0x0f
-    # define LOW_BYTE_REG 0x10
+uint16_t read_distance_measurement() {
+    uint8_t reg = 0x8f; // 0x0f med bit 7 satt för auto-increment
+    uint8_t data[2];
+
+    i2c_master_transmit_receive(lidar_dev, &reg, 1, data, 2, TIMEOUT_MS);
     
-    uint8_t data[2] = {0};
-    read_from_lidar_register(HIGH_BYTE_REG, data, 1);
-    read_from_lidar_register(LOW_BYTE_REG, data + 1, 1);
     return (data[0] << 8) | data[1];
 }
 uint16_t start_and_read_distance_measurement(bool bias_correction){
@@ -43,41 +44,43 @@ uint16_t start_and_read_distance_measurement(bool bias_correction){
     vTaskDelay(pdMS_TO_TICKS(20));  // Wait for measurement to complete
     return read_distance_measurement();
 }
+uint8_t read_lidar_status(){
+    # define STATUS_REG 0x01
+    return read_from_lidar_register(STATUS_REG);
+}
+
+// Format an 8-bit value as a binary string and return it
+static std::string format_binary8(uint8_t value) {
+    std::string out(8, '0');
+    for (int i = 0; i < 8; ++i) {
+        out[7 - i] = (value & (1 << i)) ? '1' : '0';
+    }
+    return out;
+}
 
 
 
 // ISR for LIDAR measurement ready interrupt
 static void IRAM_ATTR lidar_measurement_isr_handler(void* arg) {
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-    // Väcker tasken genom att ge semaphoren
+
     xSemaphoreGiveFromISR(sensor_sem, &xHigherPriorityTaskWoken);
     if (xHigherPriorityTaskWoken) {
         portYIELD_FROM_ISR();
     }
 }
 void sensor_read_task(void *pvParameters) {
-    ESP_LOGI(TAG, "Sensor read task started");
-
     while (1) {
-        // Vi väntar 200ms istället för 1000ms för att vara mer aggressiva
+        start_distance_measurement(false);
+
         if (xSemaphoreTake(sensor_sem, pdMS_TO_TICKS(200)) == pdTRUE) {
             uint16_t distance = read_distance_measurement();
-            ESP_LOGI(TAG, "Distance: %d cm", distance); 
+            ESP_LOGI(TAG, "%d cm", distance); 
         }
         else {
             uint16_t distance = read_distance_measurement();
-            ESP_LOGI(TAG, "Distance: %d cm", distance); 
-            // uint8_t status = 0;
-            // read_from_lidar_register(0x01, &status, 1);
-            
-            // if (status & 0x01) { // Om den fortfarande är BUSY
-            //     // Tvinga en ny mätning genom att skriva till 0x00
-            //     // Detta kan ofta "sparka igång" en hängd sensor
-            //     start_distance_measurement(true); 
-            //     ESP_LOGW(TAG, "Sensor was busy, sent new trigger. Status was: 0x%02x", status);
-            // } else {
-            //     ESP_LOGI(TAG, "LIDAR Idle, status: 0x%02x", status);
-            // }
+            uint8_t status = read_lidar_status();
+            ESP_LOGW(TAG, "Timeout. Distance: %d cm.   Status: %s   Monitor pin: %d", distance, format_binary8(status).c_str(), gpio_get_level(LIDAR_INTERRUPT_PIN));
         }
     }
 }
@@ -85,23 +88,7 @@ void sensor_read_task(void *pvParameters) {
 
 
 void init_lidar(uint32_t stack_size, uint8_t priority, uint8_t core_id) {
-    // Configure ISR for LIDAR interrupts
-    sensor_sem = xSemaphoreCreateBinary();
-    gpio_set_direction(LIDAR_INTERRUPT_PIN, GPIO_MODE_INPUT);
-    gpio_set_intr_type(LIDAR_INTERRUPT_PIN, GPIO_INTR_NEGEDGE);
-    gpio_set_pull_mode(LIDAR_INTERRUPT_PIN, GPIO_FLOATING);
-    gpio_isr_handler_add(LIDAR_INTERRUPT_PIN, lidar_measurement_isr_handler, NULL);
-
-    // Create task for reading sensor data
-    xTaskCreatePinnedToCore(
-        &sensor_read_task,
-        "sensor_read_task",
-        stack_size,
-        NULL,
-        priority,
-        NULL,
-        core_id
-    );
+    ESP_LOGI(TAG, "Initializing LIDAR...");
 
     // Configure I2C for LIDAR
     i2c_master_bus_config_t bus_config = {};
@@ -120,47 +107,49 @@ void init_lidar(uint32_t stack_size, uint8_t priority, uint8_t core_id) {
         .flags = {}
     };
     ESP_ERROR_CHECK(i2c_master_bus_add_device(i2c_bus, &dev_config, &lidar_dev));
-
-    // // Reset LIDAR registers
-    // write_to_lidar_register(0x00, 0x00);
-
-    // vTaskDelay(pdMS_TO_TICKS(10)); // Wait for reset to complete
-
-    // // Configure LIDAR registers
-    // # define ACQ_CONFIG_REG 0x04
-    // // uint8_t new_acq_config = (0b1111 << 4) | 0b0101;
-    // uint8_t new_acq_config = 0x21;
-    // write_to_lidar_register(ACQ_CONFIG_REG, new_acq_config);
-    // # define MEASURE_DELAY_REG 0x45
-    // write_to_lidar_register(MEASURE_DELAY_REG, 0);          // No delay between measurements
-    // # define OUTER_LOOP_COUNT_REG 0x11
-    // write_to_lidar_register(OUTER_LOOP_COUNT_REG, 0xff);    // Enable continous measurements
-    // // Register 0x1c: Sätt bit 6 till 0 för att pinnen ska visa "Data Ready" 
-    // // (istället för tröskelvärde)
-    // write_to_lidar_register(0x1c, 0x00);
-
-
-    // 2. Fullständig Reset och rejäl paus
-    write_to_lidar_register(0x00, 0x00); 
-    vTaskDelay(pdMS_TO_TICKS(50)); // Ge sensorn tid att starta om helt
-
-    // 3. Konfiguration - Back to Basics
     
-    // ACQ_CONFIG (0x04): 
-    // Bit 0:1 = 01 (Status Output Mode för interrupt)
-    // Vi lämnar Bit 5 som 0 (Använd standard-delay istället för register 0x45)
-    // Detta gör mätningen mer stabil.
-    uint8_t new_acq_config = (0b0000 << 4) | 0b0001;
-    write_to_lidar_register(0x04, new_acq_config); 
-
-    // OUTER_LOOP_COUNT (0x11): 0xff för kontinuerlig mätning
-    write_to_lidar_register(0x11, 0xff); 
-
-    // Vi hoppar över att skriva till 0x45 (Delay) och 0x1c (Threshold) 
-    // för att låta sensorn köra i sitt mest stabila standardläge.
+    // Reset LIDAR registers
+    vTaskDelay(pdMS_TO_TICKS(100));
+    write_to_lidar_register(0x00, 0x00);
+    ESP_LOGI(TAG, "LIDAR SERIAL NUMBER: %d", (read_from_lidar_register(0x16) << 8) | read_from_lidar_register(0x17));
+    vTaskDelay(pdMS_TO_TICKS(100));
 
 
+    // Define LIDAR register addresses
+    # define ACQ_CONFIG_REG 0x04
+    # define MEASURE_DELAY_REG 0x45
+    # define OUTER_LOOP_COUNT_REG 0x11
+    # define REF_COUNT_VAL_REG 0x12
+    # define THRESHOLD_BYPASS_REG 0x1c
 
-    // Start measurement task
-    start_distance_measurement(true);
+    // Configure LIDAR registers
+    uint8_t new_acq_config = 0;
+    new_acq_config |= 0b01; // Status output mode
+    // new_acq_config |= (1 << 5);
+    write_to_lidar_register(ACQ_CONFIG_REG, new_acq_config);    ESP_LOGI(TAG, "ACQ Config set to %s", format_binary8(read_from_lidar_register(ACQ_CONFIG_REG)).c_str());
+    // write_to_lidar_register(MEASURE_DELAY_REG, 0);              ESP_LOGI(TAG, "MEASURE_DELAY_REG set to %d", read_from_lidar_register(MEASURE_DELAY_REG));
+    write_to_lidar_register(OUTER_LOOP_COUNT_REG, 0x00);        ESP_LOGI(TAG, "OUTER_LOOP_COUNT_REG set to %d", read_from_lidar_register(OUTER_LOOP_COUNT_REG));
+    
+    // Configure GPIO and ISR for LIDAR interrupts
+    sensor_sem = xSemaphoreCreateBinary();
+    gpio_install_isr_service(0);
+    gpio_set_direction(LIDAR_INTERRUPT_PIN, GPIO_MODE_INPUT);
+    gpio_set_intr_type(LIDAR_INTERRUPT_PIN, GPIO_INTR_NEGEDGE);
+    gpio_set_pull_mode(LIDAR_INTERRUPT_PIN, GPIO_FLOATING);
+    gpio_isr_handler_add(LIDAR_INTERRUPT_PIN, lidar_measurement_isr_handler, NULL);
+
+    vTaskDelay(pdMS_TO_TICKS(100));
+
+    // Create task for reading after interrupts
+    xTaskCreatePinnedToCore(
+        &sensor_read_task,
+        "sensor_read_task",
+        stack_size,
+        NULL,
+        priority,
+        NULL,
+        core_id
+    );
+
+    ESP_LOGI(TAG, "init_lidar complete");
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,32 @@
 
 static const char *TAG = "MAIN";
 
+
+// void test_task(void *pvParameters) {
+//     ESP_LOGI(TAG, "Test task started");
+//     gpio_set_direction(GPIO_NUM_13, GPIO_MODE_INPUT);
+//     gpio_set_pull_mode(GPIO_NUM_13, GPIO_FLOATING);
+
+//     while (1) {
+//         if (gpio_get_level(GPIO_NUM_13) == 0) {
+//             ESP_LOGI(TAG, "GPIO 13 Interrupted");
+//             vTaskDelay(pdMS_TO_TICKS(100));
+//         }
+//         taskYIELD();
+//     }
+// }
+
+
 extern "C" void app_main() {
-    gpio_install_isr_service(0);
 
     init_lidar(2048, 5, 1);
+    // xTaskCreatePinnedToCore(
+    //     &test_task,
+    //     "test_task",
+    //     2048,
+    //     NULL,
+    //     0,
+    //     NULL,
+    //     0
+    // );
 }
